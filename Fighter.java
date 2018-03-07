5efd66c4c3b711d0428e59f8ce580fceb1cab0c9
Marc Frankel <mfrankel218@gmail.com>
2018-03-06 20:42:18 -0500

    Added fighter java docs

--- Fighter.java
+++ Fighter.java
@@ -1,15 +1,39 @@
+/**
+ * Class representing the sub class of a fighting character in the game
+ * @author mfrankel8
+ * @version 1
+ */
 public class Fighter extends Character {
 
+    /**
+     * Fighter contructor that takes full params
+     * @param  name         Name of fighter
+     * @param  level        Level of fighter
+     * @param  strength     strenth of fighter
+     * @param  dexterity    dexterity of fighter
+     * @param  intelligence intelligence of fighter
+     * @param  wisdom       wisdom of fighter
+     *
+     */
     public Fighter(String name, int level,
                      int strength, int dexterity,
                      int intelligence, int wisdom) {
         super(name, level, strength, dexterity, intelligence, wisdom);
     }
 
+    /**
+     * Fighter constructor for min params
+     * @param  name Name of fighter
+     * @param  seed seed to generate other levels
+     *
+     */
     public Fighter(String name, int seed) {
         super(name, seed);
     }
 
+    /**
+     * level up function for the fighter class
+     */
     public void levelUp() {
         setLevel(getLevel() + 1);
         setHealth(getLevel() * 5);
@@ -19,7 +43,10 @@ public class Fighter extends Character {
         setWisdom(getWisdom() + 1);
     }
 
-
+    /**
+     * toString function for a fighter object
+     * @return String representation
+     */
     public String toString() {
         return String.format("Level %s fighter named %s with %s strength, "
             + "%s dexterity, %s intelligence, and %s wisdom.", getLevel(),
@@ -27,6 +54,10 @@ public class Fighter extends Character {
             getWisdom());
     }
 
+    /**
+     * Attach function of fighter
+     * @param c character to attack
+     */
     public void attack(Character c) {
         if (c.getIsDead()) {
             System.out.println("Cannot attack a dead character");
