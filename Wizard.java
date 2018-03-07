b4830aea8aeb25308d8edaa74f0e2a5aa08d2d47
Marc Frankel <mfrankel218@gmail.com>
2018-03-06 21:36:45 -0500

    Completed Wizard javadocs

--- Wizard.java
+++ Wizard.java
@@ -1,14 +1,39 @@
+/**
+ * Class representing the sub class of a Wizard character in the game
+ * @author mfrankel8
+ * @version 1
+ */
 public class Wizard extends Character {
+
+    /**
+     * Main wizard constructor
+     * @param  name         Name of Wizard
+     * @param  level        Level of wizard
+     * @param  strength     Stength of wizard
+     * @param  dexterity    Dexterity of wizard
+     * @param  intelligence Intelligence of wizard
+     * @param  wisdom       Wisdom of wizard
+     *
+     */
     public Wizard(String name, int level,
                      int strength, int dexterity,
                      int intelligence, int wisdom) {
         super(name, level, strength, dexterity, intelligence, wisdom);
     }
 
+    /**
+     * Second Wizard constructor
+     * @param  name Name of wizard
+     * @param  seed Random seed for wizard
+     *
+     */
     public Wizard(String name, int seed) {
         super(name, seed);
     }
 
+    /**
+     * Level up function for Wizard
+     */
     public void levelUp() {
         setLevel(getLevel() + 1);
         setHealth(getLevel() * 5);
@@ -18,6 +43,10 @@ public class Wizard extends Character {
         setWisdom(getWisdom() + 1);
     }
 
+    /**
+     * toString function for Rogue
+     * @return string representation
+     */
     public String toString() {
         return String.format("Level %s wizard named %s with %s strength, "
             + "%s dexterity, %s intelligence, and %s wisdom.", getLevel(),
@@ -25,6 +54,10 @@ public class Wizard extends Character {
             getWisdom());
     }
 
+    /**
+     * MultiAttack function for rogue
+     * @param c chars to attack
+     */
     public void multiAttack(Character ... c) {
         for (Character target: c) {
             if (target.getIsDead()) {
@@ -35,6 +68,10 @@ public class Wizard extends Character {
         }
     }
 
+    /**
+     * Attack function for Wizard
+     * @param c char to attack
+     */
     public void attack(Character c) {
         if (c.getIsDead()) {
             System.out.println("Cannot attack a dead character");
